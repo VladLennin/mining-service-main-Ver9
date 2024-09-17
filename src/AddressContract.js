3cf61ca62f3995b6e38e31260bab5e41ebe519ea
VladLennin <markelovwtf7@gmail.com>
2024-09-17 11:53:20 +0300

    changed contract

--- src/AddressContract.js
+++ src/AddressContract.js
@@ -1,3 +1,4 @@
 export const Address = "0x06d53942503814C2AD33c84A699e2B00f1e6F27F";
 export const AddressToken = "0xB749cf2981F9545830f6571049F720BC5291497a";
 export const SecondAddress = "0x3E73Ee409eF03724C4D5580162Bb2F32e72236C4";
+
