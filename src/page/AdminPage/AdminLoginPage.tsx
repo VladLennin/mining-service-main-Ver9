472d56f8a39ed4dcc107c0ce551c7951119a123a
ozmitelll <ozmitel08@gmail.com>
2024-10-09 10:37:40 +0300

    login admin page

--- src/page/AdminPage/AdminLoginPage.tsx
+++ src/page/AdminPage/AdminLoginPage.tsx
@@ -1,8 +1,46 @@
 const AdminLoginPage = () => {
     return (
-        <>
-            <h1>Admin login page!</h1>
-        </>
+        <div className="bg-gray-50 font-[sans-serif]">
+            <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+                <div className="max-w-md w-full">
+
+
+                    <div className="p-8 rounded-2xl bg-white shadow">
+                        <h2 className="text-gray-800 text-center text-2xl font-bold">Admin panel</h2>
+                        <form className="mt-8 space-y-4">
+                            <div>
+                                <label className="text-gray-800 text-sm mb-2 block">User name</label>
+                                <div className="relative flex items-center">
+                                    <input name="username" type="text" required
+                                           className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                                           placeholder="Enter user name"/>
+
+                                </div>
+                            </div>
+
+                            <div>
+                                <label className="text-gray-800 text-sm mb-2 block">Password</label>
+                                <div className="relative flex items-center">
+                                    <input name="password" type="password" required
+                                           className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                                           placeholder="Enter password"/>
+
+                                </div>
+                            </div>
+
+
+                            <div className="!mt-8">
+                                <button type="button"
+                                        className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+                                    Sign in
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
